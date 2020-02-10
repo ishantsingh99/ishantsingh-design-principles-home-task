5ec6d681d8455b6971b424a205db4a55078b5d3a
ishantsingh99 <44225391+ishantsingh99@users.noreply.github.com>
2020-02-10 18:20:04 +0530

    Add files via upload

---
+++ solid.java
@@ -0,0 +1,25 @@
+public class solid {
+    static void add(int a,int b){
+        int s=a+b;
+        System.out.println(s);
+    }
+   static void subtract(int a,int b){
+        int s=a-b;
+        System.out.println(s);
+    }
+    static void multiply(int a,int b){
+        int m=a*b;
+        System.out.println(m);
+    }
+    static void divide(int a,int b){
+        int d=a/b;
+        System.out.println(d);
+    }
+    public static void main(String args[])
+    {
+        add(2,6);
+        subtract(5,3);
+        multiply(4,5);
+        divide(6,3);
+    }
+}
---
+++ yagni.java
@@ -0,0 +1,24 @@
+public class yagni {
+    static void calculator(String s,int a,int b)
+    {
+        switch(s)
+        {
+            case "add":
+                System.out.println(a+b);
+                break;
+            case "subtract":
+                System.out.println(a-b);
+                break;
+            case "multiply":
+                System.out.println(a*b);
+                break;
+            case "divide":
+                System.out.println(a/b);
+                break;
+        }
+    }
+    public static void main(String args[]){
+        calculator("add",2,4);
+        calculator("multiply",3,6);
+    }
+}
